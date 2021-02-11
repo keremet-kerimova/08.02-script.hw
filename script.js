d1043a810952a8221664579ddb5f89e1152d4083
Your Name <you@example.com>
2021-02-11 16:30:25 +0600

    changing codes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let inp2 = document.querySelector('#password');
 let p = document.querySelector('.p');
 
 
-inp2.addEventListener('input', function(even){
+inp2.addEventListener('input', function(event){
     if (inp2.value.length == 0){
         p.innerText = 'Enter password';
     }
@@ -12,25 +12,26 @@ inp2.addEventListener('input', function(even){
     if (inp2.value.length < 6){
         p.innerText = 'Good';
     }
-    else if (inp2.value.length < 10){
+    else if (inp2.value.length < 8){
         p.innerText = 'Very good';
     }
+    
 });
 
 
 
-let input2 = document.querySelector('#number');
-let button = document.querySelector('#number2');
+let input2 = document.querySelector('#number1');
+let button = document.querySelector('#button');
 let p2 = document.querySelector('#p2');
 
 
-let dice = (Math.random () * 6) + 1;
+let dice = Math.floor(Math.random () * 6) + 1;
 button.addEventListener('click',function (event){
      if (input2 == dice){
          p2.innerText = 'You win ! Number is ' + dice;
      }
-     else if (input2 !=dice){
-         p2.innerText = 'You won ! Number is ' + dice;
+     else if (input2 != dice){
+         p2.innerText = 'You lose ! Number is ' + dice;
      }
     
 });
