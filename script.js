e19a1ba71cc226f474f0e85440633acb5be90177
Your Name <you@example.com>
2021-02-11 15:38:31 +0600

    added password codes

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-fullName.addEventListener('change', function(event) { // при нажатии на клавищу
-    console.log('change');
-    //    if (event.code.match(/Key/) && !event.key.match(/[а-яА-Я]/)) {
-    //      event.preventDefault();
-    //  }
-     if (fullName.value.length > 5) {
-      console.log('Good')
-     }
-     else ('very good')
-});
-for (ix = 0; ix < 100; ix++){
-    ix2 = i + 1;
-    if (ix2 >= 100 - 1){
-        ix2 = 0;
-        ix2 = 0;
+let in2 = document.querySelector('#password');
+let p = document.querySelector('p');
+
+
+in2.addEventListener('input', function(even){
+    if (in2.nodeValue.length == 0){
+        p.innerText = ' Ввыведите пароль';
     }
-    sum = ix + ix2 ;
-    printlinwe(sum)
-}
+})
+
+
+
+// fullName.addEventListener('change', function(event) { // при нажатии на клавищу
+//     console.log('change');
+//     //    if (event.code.match(/Key/) && !event.key.match(/[а-яА-Я]/)) {
+//     //      event.preventDefault();
+//     //  }
+//      if (fullName.value.length > 5) {
+//       console.log('Good')
+//      }
+//      else ('very good')
+// });
+// for (ix = 0; ix < 100; ix++){
+//     ix2 = i + 1;
+//     if (ix2 >= 100 - 1){
+//         ix2 = 0;
+//         ix2 = 0;
+//     }
+//     sum = ix + ix2 ;
+//     printlinwe(sum)
+// }
